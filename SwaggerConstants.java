fa7b89792fa0a9be45062657c79c0dded7b317a0
StenioVilar <31133021+StenioVilar@users.noreply.github.com>
2018-10-16 15:13:51 -0300

    Update SwaggerConstants.java

--- SwaggerConstants.java
+++ SwaggerConstants.java
@@ -1,5 +1,8 @@
+package main;
+
 public class SwaggerConstantes {
 	
-	public static final String URL_SWAGGER_FUNCTIONALITY_STATE  	= "FUNCIONALITY-STATE";
+	public static final String URL_SWAGGER_VARIABLE_NAME					  	= "VARIABLE_NAME"; //OBTÉM VARIÁVEL AMBIENTE
+	//       														  			<property name="VARIABLE_NAME" value="*********"/>
 
 }
